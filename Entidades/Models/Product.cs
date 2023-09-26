3a26f4420a57d56775a6b32b2a3a82c12547e39a
Haruto-ZX <103148687+Haruto-ZX@users.noreply.github.com>
2023-09-26 16:43:14 -0400

    Update Product.cs

--- Entidades/Models/Product.cs
+++ Entidades/Models/Product.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//esta parte la hize
 namespace Entidades.Models
 {
     public class Product
     {
+        //id del producto
         public int Id { get; set; } 
+        //nombre del producto
         public string Nombre { get; set; } 
+        // precio del producto
         public decimal Precio { get; set; } 
     }
 }
